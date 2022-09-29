a73a9b34637b811fa79288892c618c4b1a65ff5c
DragonBanana <10493858@Polimi.it>
2022-09-29 15:25:19 +0200

    updated login with side to side buttons

--- flutter/login/lib/main.dart
+++ flutter/login/lib/main.dart
@@ -48,15 +48,31 @@ class StatelessLogin extends StatelessWidget {
           keyboardType: TextInputType.multiline,
           maxLines: null,
         ),
-        TextButton(
-          onPressed: () => showDialog(
-              context: context,
-              builder: (context) => LoginDialog(
-                    usernameController.text,
-                    passwordController.text,
-                  )),
-          child: const Text("Login"),
-        ),
+        // To place component side by side inside the same row
+        Row(children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) => RegisterDialog(
+                        usernameController.text,
+                        passwordController.text,
+                      )),
+              child: const Text("Register"),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) => LoginDialog(
+                        usernameController.text,
+                        passwordController.text,
+                      )),
+              child: const Text("Login"),
+            ),
+          ),
+        ]),
       ],
     );
   }
@@ -72,7 +88,33 @@ class LoginDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Login dialog'),
-      content: Text('$username tried to login with this password $password'),
+      content: Text('$username tried to login with password: $password'),
+      actions: <Widget>[
+        TextButton(
+          style: TextButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.labelLarge,
+          ),
+          child: const Text('Ok'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class RegisterDialog extends StatelessWidget {
+  final String username;
+  final String password;
+
+  const RegisterDialog(this.username, this.password, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Register dialog'),
+      content: Text('trying to register $username with password: $password'),
       actions: <Widget>[
         TextButton(
           style: TextButton.styleFrom(
