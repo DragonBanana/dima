790c5859e5fd4d9b8b02f581837e4c4587fd209c
DragonBanana <10493858@Polimi.it>
2022-09-29 15:13:54 +0200

    added login

--- flutter/viewer/lib/main.dart
+++ flutter/viewer/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image viewer',
       home: Scaffold(
         appBar: AppBar(title: const Text("Image viewer")),
         body: Main(),
