08ca03df2650eba1daa9e1da24f0a79b3df2c5bb
DragonBanana <davideyi.hu@polimi.it>
2022-10-21 16:56:51 +0200

    added calculator

--- flutter/counter/lib/main.dart
+++ flutter/counter/lib/main.dart
@@ -5,111 +5,129 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        home: Scaffold(
+            appBar: AppBar(title: const Text("Counter")),
+            body: const Counter()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // A stateful widget must
+  // override the createState method
+  CounterState createState() => CounterState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CounterState extends State<Counter> {
+  int counter = 0;
 
-  void _incrementCounter() {
+  void incrementCounter() {
+    // Whenever the state changes
+    // it is mandatory to call "setState"
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (counter < 100) {
+        // Inside setState 
+        // insert only the code that changes the state
+        counter++;
+      }
+    });
+  }
+
+  void decrementCounter() {
+    setState(() {
+      if (counter > 0) {
+        counter--;
+      }
+    });
+  }
+
+  void setCounter(double newValue) {
+    setState(() {
+      counter = newValue.toInt();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Text("$counter",
+                      style: const TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white)),
+                )),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.pinkAccent),
+                child: const Icon(Icons.remove),
+                // By reading the documentation
+                // we can find that, to disable the textbutton,
+                // we can just set "onPressed" to null
+                onPressed: counter == 0 ? null : decrementCounter,
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.pinkAccent),
+                child: const Icon(Icons.add),
+                onPressed: counter == 100 ? null : incrementCounter,
+              )
+            ]),
+            // Slider
+            // If the value of the slider overflowers
+            // an error is generated
+            Slider(
+                activeColor: Colors.blueAccent,
+                inactiveColor: Colors.blueGrey,
+                value: counter.toDouble(),
+                min: 0,
+                max: 100,
+                divisions: 100,
+                // onChanged callback
+                onChanged: setCounter),
+            // Ternary operator
+            // <condition> ? <if_true> : <if_false>
+            // In this case, we display a column if counter < 100
+            // Otherwise we display a text
+            counter < 100
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // ProgressIndicator
+                      // Overflow does not make it crash
+                      // We should be careful when we are
+                      // dealing with illegal values
+                      // Alternatives are:
+                      // CircularProgressIndicator, RefreshProgressIndicator
+                      LinearProgressIndicator(
+                        backgroundColor: Colors.grey,
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.red),
+                        value: counter.toDouble() / 100,
+                      ),
+                      Text('${(counter.toDouble()).round()}%'),
+                    ],
+                  )
+                : const Text("Banana!")
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ));
   }
 }
