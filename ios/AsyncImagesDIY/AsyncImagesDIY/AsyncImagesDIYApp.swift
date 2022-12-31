545b2fdc435c8e3ddd3f99dccfdf33d47fe95c76
Luca Terracciano <terraccianoluca@gmail.com>
2022-12-31 13:24:31 +0100

    feat: lecture-8

---
+++ ios/AsyncImagesDIY/AsyncImagesDIY/AsyncImagesDIYApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImagesDIYApp.swift
+//  AsyncImagesDIY
+//
+//  Created by Luca Terracciano on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImagesDIYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
